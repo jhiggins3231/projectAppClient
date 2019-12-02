7525da61fe93abbdc4ad13eeffc2e0e52dafacbb
James <jhiggins3231@gmail.com>
2019-12-02 10:55:22 -0500

    disregard

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
   production: true
 };
+
+export let APIURL = 'http://localhost:3000/';
+ 
+switch (window.location.hostname) {
+  case 'localhost:3000':
+    APIURL = 'http://localhost:3000'
+    break
+    default:
+      APIURL = 'http://localhost:3000'
+}
+ 
